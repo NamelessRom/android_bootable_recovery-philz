6abab626e6dd1b07ba713f532b6c533ea0d87864
Evisceration <eviscerationls@gmail.com>
2014-10-28 22:30:33 +0100

    default to koush' background
    Change-Id: If6933fbc0654509961ce0f958a4b2442d8c7a2dd

--- device_images/Android.mk
+++ device_images/Android.mk
@@ -2,24 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 # device background image
 include $(CLEAR_VARS)
-ifdef PHILZ_TOUCH_RECOVERY
-    #stitch.png
-    LOCAL_MODULE := stitch.png
-    LOCAL_MODULE_TAGS := optional
-    LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
-    LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/res/images
-    LOCAL_SRC_FILES := bg/$(TARGET_SCREEN_WIDTH)x$(TARGET_SCREEN_HEIGHT)_bg.png
-
-    #other files go here
-
-else
-    #stitch.png
-    LOCAL_MODULE := stitch.png
-    LOCAL_MODULE_TAGS := optional
-    LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
-    LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/res/images
-    LOCAL_SRC_FILES := bg/koush.png
-endif
+LOCAL_MODULE := stitch.png
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/res/images
+LOCAL_SRC_FILES := bg/koush.png
 include $(BUILD_PREBUILT)
 
 
